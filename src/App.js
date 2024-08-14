8384d7264a030209e27c01bf111a4e6270fd342c
Alex King <48763366+Mel0nHead@users.noreply.github.com>
2024-08-14 08:48:49 +0100

    should display the user's turn

--- src/App.js
+++ src/App.js
@@ -7,23 +7,26 @@ function App() {
   return (
     <div style={{ display: "flex", justifyContent: "center", marginTop: 100 }}>
       <div>
-        {initialiseGrid().map((row, i) => {
-          return (
-            <div style={{ display: "flex" }} key={i}>
-              {row.map((cell) => (
-                <div
-                  key={cell}
-                  style={{
-                    border: "1px solid",
-                    height: cellSize,
-                    width: cellSize,
-                  }}
-                  data-testid="grid-cell"
-                ></div>
-              ))}
-            </div>
-          );
-        })}
+        <p>Current turn: red</p>
+        <div>
+          {initialiseGrid().map((row, i) => {
+            return (
+              <div style={{ display: "flex" }} key={i}>
+                {row.map((cell) => (
+                  <div
+                    key={cell}
+                    style={{
+                      border: "1px solid",
+                      height: cellSize,
+                      width: cellSize,
+                    }}
+                    data-testid="grid-cell"
+                  ></div>
+                ))}
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", async () => {
+test("should display the correct number of grid cells", async () => {
   render(<App />);
   const gridCells = await screen.findAllByTestId("grid-cell");
   expect(gridCells).toHaveLength(6 * 7);
+});
+
+test("should display who's turn is next", () => {
+  render(<App />);
+
+  expect(screen.getByText(/Current turn: red/));
 });
