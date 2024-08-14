18bb39cd140db4425182e7e1eb3124bb3404eaf7
Alex King <48763366+Mel0nHead@users.noreply.github.com>
2024-08-14 08:17:42 +0100

    add 6 x 7 grid

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const gridCells = await screen.findAllByTestId("grid-cell");
+  expect(gridCells).toHaveLength(6 * 7);
 });
