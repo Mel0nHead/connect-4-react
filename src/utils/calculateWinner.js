5e08bf7084ab001889e53253ed7f9b34477ed64f
Alex King <48763366+Mel0nHead@users.noreply.github.com>
2024-08-20 08:34:24 +0100

    calculate row-based winner

--- src/utils/calculateWinner.js
+++ src/utils/calculateWinner.js
@@ -1,19 +1,45 @@
 import { NUMBER_OF_COLUMNS } from "../constants";
 
+const winningNumber = 4;
+
+function hasRowBasedWinner(gameState, indexOfLastMove, valueOfLastMove) {
+  const consecutiveSquaresInRow = gameState
+    .filter(
+      ([_, i]) =>
+        Math.floor(i / NUMBER_OF_COLUMNS) ===
+        Math.floor(indexOfLastMove / NUMBER_OF_COLUMNS)
+    )
+    .reduce((count, [cellValue, _]) => {
+      if (count === winningNumber) return count;
+      return cellValue === valueOfLastMove ? count + 1 : 0;
+    }, 0);
+
+  return consecutiveSquaresInRow === winningNumber;
+}
+
+function hasColumnBasedWinner(gameState, indexOfLastMove, valueOfLastMove) {
+  const consecutiveSquaresInColumn = gameState
+    .filter(([_, i]) => i % 7 === indexOfLastMove % NUMBER_OF_COLUMNS)
+    .reduce((count, [cellValue, _]) => {
+      if (count === winningNumber) return count;
+
+      return cellValue === valueOfLastMove ? count + 1 : 0;
+    }, 0);
+
+  return consecutiveSquaresInColumn === winningNumber;
+}
+
 function calculateWinner(gameState, indexOfLastMove) {
   const valueOfLastMove = gameState[indexOfLastMove];
+  const gameStateWithIndexes = gameState.map((value, i) => [value, i]);
 
-  const column = gameState
-    .map((value, i) => [value, i])
-    .filter(([_, i]) => i % 7 === indexOfLastMove % NUMBER_OF_COLUMNS);
-
-  const consecutiveCellCount = column.reduce((count, [cellValue, _]) => {
-    if (count === 4) return count;
-
-    return cellValue === valueOfLastMove ? count + 1 : 0;
-  }, 0);
-
-  return consecutiveCellCount === 4 ? valueOfLastMove : null;
+  return hasColumnBasedWinner(
+    gameStateWithIndexes,
+    indexOfLastMove,
+    valueOfLastMove
+  ) || hasRowBasedWinner(gameStateWithIndexes, indexOfLastMove, valueOfLastMove)
+    ? valueOfLastMove
+    : null;
 }
 
 export default calculateWinner;
