1fb1e56d934a483dc136e2dab2e3c8fba978edc7
Alex King <48763366+Mel0nHead@users.noreply.github.com>
2024-08-24 10:41:06 +0100

    get ascending diagonal working

--- src/utils/calculateWinner.js
+++ src/utils/calculateWinner.js
@@ -1,3 +1,5 @@
+import { NUMBER_OF_COLUMNS, NUMBER_OF_ROWS } from "../constants";
+
 const winningNumber = 4;
 
 function calculateWinner(gameState, move) {
@@ -22,7 +24,19 @@ function calculateWinner(gameState, move) {
       return cellValue === valueOfLastMove ? count + 1 : 0;
     }, 0);
 
-  return consecutiveSquaresInColumn === winningNumber ||
+  const increasingDialognal = Array.from(Array(NUMBER_OF_COLUMNS).keys())
+    .map((value) => [value, columnIndex + rowIndex - value])
+    .filter(([_, rowIndex]) => rowIndex < NUMBER_OF_ROWS);
+
+  const x = increasingDialognal.reduce((count, [colIndex, rowIndex]) => {
+    const cellValue = gameState[colIndex][rowIndex];
+    if (count === winningNumber) return count;
+
+    return cellValue === valueOfLastMove ? count + 1 : 0;
+  }, 0);
+
+  return x === winningNumber ||
+    consecutiveSquaresInColumn === winningNumber ||
     consecutiveSquaresInRow === winningNumber
     ? valueOfLastMove
     : null;
