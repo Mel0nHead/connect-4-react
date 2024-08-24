614642af578d84044468e81e7d414fd2e87771fd
Alex King <48763366+Mel0nHead@users.noreply.github.com>
2024-08-24 09:36:59 +0100

    get row based calculation working

--- src/utils/calculateWinner.js
+++ src/utils/calculateWinner.js
@@ -1,21 +1,4 @@
-import { NUMBER_OF_COLUMNS } from "../constants";
-
 const winningNumber = 4;
-
-function hasRowBasedWinner(gameState, indexOfLastMove, valueOfLastMove) {
-  const consecutiveSquaresInRow = gameState
-    .filter(
-      ([_, i]) =>
-        Math.floor(i / NUMBER_OF_COLUMNS) ===
-        Math.floor(indexOfLastMove / NUMBER_OF_COLUMNS)
-    )
-    .reduce((count, [cellValue, _]) => {
-      if (count === winningNumber) return count;
-      return cellValue === valueOfLastMove ? count + 1 : 0;
-    }, 0);
-
-  return consecutiveSquaresInRow === winningNumber;
-}
 
 function calculateWinner(gameState, move) {
   const [columnIndex, rowIndex] = move;
@@ -31,7 +14,18 @@ function calculateWinner(gameState, move) {
     0
   );
 
-  return consecutiveSquaresInColumn === winningNumber ? valueOfLastMove : null;
+  const consecutiveSquaresInRow = gameState
+    .map((col) => col[rowIndex])
+    .reduce((count, cellValue) => {
+      if (count === winningNumber) return count;
+
+      return cellValue === valueOfLastMove ? count + 1 : 0;
+    }, 0);
+
+  return consecutiveSquaresInColumn === winningNumber ||
+    consecutiveSquaresInRow === winningNumber
+    ? valueOfLastMove
+    : null;
 }
 
 export default calculateWinner;
