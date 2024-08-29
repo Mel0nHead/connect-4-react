d188fa9374981ff7656a7021e065e3498f487b12
Alex King <48763366+Mel0nHead@users.noreply.github.com>
2024-08-29 09:09:03 +0100

    should W in GridCell if isWinningCell is true

--- src/components/GridCell.js
+++ src/components/GridCell.js
@@ -1,6 +1,13 @@
 const CELL_SIZE = 60;
 
-function GridCell({ onClick, cellIndex, value, columnIndex, rowIndex }) {
+function GridCell({
+  onClick,
+  cellIndex,
+  value,
+  columnIndex,
+  rowIndex,
+  isWinningCell,
+}) {
   return (
     <div
       role="button"
@@ -16,6 +23,7 @@ function GridCell({ onClick, cellIndex, value, columnIndex, rowIndex }) {
       <span>
         {columnIndex}, {rowIndex}
       </span>
+      {isWinningCell ? <span>W</span> : null}
       {value ? <span style={{ display: "none" }}>{value}</span> : null}
     </div>
   );
--- src/components/GridCell.test.js
+++ src/components/GridCell.test.js
@@ -9,6 +9,7 @@ test("should not show a value if value is null", async () => {
       columnIndex={0}
       rowIndex={0}
       value={null}
+      isWinningCell={false}
     />
   );
 
@@ -16,6 +17,7 @@ test("should not show a value if value is null", async () => {
 
   expect(cell).not.toHaveTextContent("red");
   expect(cell).not.toHaveTextContent("yellow");
+  expect(cell).not.toHaveTextContent("W");
 });
 
 test("should be yellow if value is yellow", async () => {
@@ -26,6 +28,7 @@ test("should be yellow if value is yellow", async () => {
       columnIndex={0}
       rowIndex={0}
       value="yellow"
+      isWinningCell={false}
     />
   );
 
@@ -42,10 +45,28 @@ test("should be red if value is red", async () => {
       columnIndex={0}
       rowIndex={0}
       value="red"
+      isWinningCell={false}
     />
   );
 
   const cell = await screen.findByTestId("grid-cell-0");
 
   expect(cell).toHaveTextContent("red");
+});
+
+test("should display 'W' if it is a winning cell", async () => {
+  render(
+    <GridCell
+      onClick={jest.fn()}
+      cellIndex={0}
+      columnIndex={0}
+      rowIndex={0}
+      value="red"
+      isWinningCell={true}
+    />
+  );
+
+  const cell = await screen.findByTestId("grid-cell-0");
+
+  expect(cell).toHaveTextContent("W");
 });
